863005423565557746643dc213fae86ae3ad7f30
Puru Vaish <puru.vaish@tno.nl>
2024-05-15 10:55:20 +0200

    change: update to also show cluster assignment in plot

--- main.py
+++ main.py
@@ -6,19 +6,34 @@ import matplotlib.pyplot as plt
 from clustering import KMeansPP, KMeans
 
 
-def create_dummy_data(n_samples=1000, centers=4, n_features=2, random_state=42):
+def create_dummy_data(n_samples=10000, centers=4, n_features=2, batch_size=8):
     from sklearn.datasets import make_blobs
 
     # Generate some data
-    X, y = make_blobs(n_samples=n_samples, centers=centers, n_features=n_features, random_state=random_state)
-    X = torch.tensor(X).float()
-    X = torch.stack([X, X + 10, X - 10], dim=0)  # dummy batch dimension
-    return X
+    batch_list = []
+    for _ in range(batch_size):
+        _x, _ = make_blobs(n_samples=n_samples, centers=centers, n_features=n_features)
+        _x = torch.tensor(_x).float()
+        batch_list.append(_x)
+    x = torch.stack(batch_list, dim=0)  # dummy batch dimension
+    return x
 
 
-def plot_kmeans_result(X, _centers):
-    plt.scatter(X[:, 0].cpu(), X[:, 1].cpu())
-    plt.scatter(_centers[:, 0].cpu(), _centers[:, 1].cpu(), c='red', marker='x')
+def plot_kmeans_result(data, _centers, _assignment, title='KMeans with KMeans++ Initialization'):
+    data = data.cpu().numpy()
+    _centers = _centers.cpu().numpy()
+    _assignment = _assignment.cpu().numpy()
+
+    max_plot = min(data.shape[0], 10)
+
+    _, axs = plt.subplots(2, round(max_plot / 2), figsize=(20, 10))
+    for i in range(max_plot):
+        ax = axs[i % 2, i // 2]
+        ax.scatter(data[i][:, 0], data[i][:, 1], c=_assignment[i], cmap='viridis')
+        ax.scatter(_centers[i][:, 0], _centers[i][:, 1], c='red', marker='x')
+
+    plt.suptitle(title)
+    plt.tight_layout()
     plt.show()
 
 
@@ -34,21 +49,24 @@ def main():
 
     kmeans = KMeans(_num_clusters, _num_features, _num_iterations)
     _centers = KMeansPP(_num_clusters, _num_features)(X)
-    _centers = kmeans(X, _centers)
+    _centers, _assignment = kmeans(X, _centers)
 
     stop = timeit.default_timer()
 
     print('Time: ', stop - start)
 
-    plot_kmeans_result(X[0], _centers[0])
+    plot_kmeans_result(
+        X, _centers, _assignment[:, :, 0],
+        title=f'KMeans with KMeans++ Initialization (Time: {stop - start:.2f} sec)'
+    )
 
 
 def main_cuda():
     print('CUDA is available:', torch.cuda.is_available())
-    X = create_dummy_data(n_samples=2500)
+    X = create_dummy_data()
     print('data shape:', X.shape)
 
-    _num_clusters = 50
+    _num_clusters = 4
     _num_dims = 2
     _num_iterations = 10
 
@@ -57,14 +75,17 @@ def main_cuda():
     X = X.cuda()
     kmeans = KMeans(_num_clusters, _num_dims, _num_iterations).cuda()
     _centers = KMeansPP(_num_clusters, _num_dims).cuda()(X)
-    _centers = kmeans(X, _centers)
+    _centers, _assignment = kmeans(X, _centers)
 
     stop = timeit.default_timer()
 
     print('Time: ', stop - start)
 
     # plot the clusters and the cluster centers
-    plot_kmeans_result(X[0], _centers[0])
+    plot_kmeans_result(
+        X, _centers, _assignment[:, :, 0],
+        title=f'KMeans with KMeans++ Initialization using CUDA (Time: {stop - start:.2f} sec)'
+    )
 
 
 if __name__ == '__main__':
