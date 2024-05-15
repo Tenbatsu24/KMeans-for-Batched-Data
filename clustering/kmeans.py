d259e0c555f848404454cbde18f15c2c3529804b
Puru Vaish <puru.vaish@tno.nl>
2024-05-15 10:54:44 +0200

    change: also returns final cluster assignment

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -15,8 +15,10 @@ class KMeans(nn.Module):
     @torch.no_grad()
     def forward(self, data, centers):
         orig_shape = data.shape
+
         if len(orig_shape) != 3:
             data = rearrange(data, 'n d -> () n d')
+            centers = rearrange(centers, 'c d -> () c d')
 
         # Step 2: Compute the distance of each data point from the nearest cluster center
         for i in range(self.num_iterations):
@@ -33,35 +35,7 @@ class KMeans(nn.Module):
             if torch.allclose(centers, old_centers):
                 break
 
-        return centers if len(orig_shape) == 3 else rearrange(centers, '() c d -> c d')
-
-
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from sklearn.datasets import make_blobs
-
-    from clustering.kmeans_pp import KMeansPP
-
-    # Generate some data
-    X, y = make_blobs(n_samples=1000, centers=4, n_features=2, random_state=42)
-    X = torch.tensor(X).float()
-
-    X = torch.stack([X, X + 10, X - 10], dim=0)  # dummy batch dimension
-    print(X.shape)
-
-    _num_clusters = 4
-    _num_features = 2
-    _num_iterations = 10
-
-    kmeans = KMeans(_num_clusters, _num_features, _num_iterations)
-    _centers = KMeansPP(_num_clusters, _num_features)(X)
-
-    _centers = kmeans(X, _centers)
-
-    # plot the clusters and the cluster centers
-    X = X[0]
-    _centers = _centers[0]
-
-    plt.scatter(X[:, 0], X[:, 1])
-    plt.scatter(_centers[:, 0], _centers[:, 1], c='red', marker='x')
-    plt.show()
+        return (
+            centers if len(orig_shape) == 3 else rearrange(centers, '() c d -> c d'),
+            cluster_assignment if len(orig_shape) == 3 else rearrange(cluster_assignment, '() b n -> b n')
+        )
